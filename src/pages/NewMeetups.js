08fed8e8cebbedab8a4327dcbd37ea8a3504e5a4
jasmeet1233 <jazzratra@gmail.com>
2021-05-15 22:08:29 +0530

    added forms

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,7 +1,17 @@
-function NewMeetupsPage(){
-    return <div> 
-        All New meetups page
-    </div>
+import NewMeetupsForms from "../components/meetups/NewMeetupForm";
+
+function NewMeetupsPage() {
+
+  function addMeetupHandler(dataUserStored){
+    fetch('https://react-project-73830-default-rtdb.asia-southeast1.firebasedatabase.app/meetips.json');
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetups</h1>
+      <NewMeetupsForms onAddMeetup = {addMeetupHandler} />
+    </section>
+  );
 }
 
 export default NewMeetupsPage;
