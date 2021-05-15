d271f676d0ce5ea282f2927f894f20e4f7ebfcac
jasmeet1233 <jazzratra@gmail.com>
2021-05-15 22:29:44 +0530

    Added api of firebase

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,15 +1,23 @@
 import NewMeetupsForms from "../components/meetups/NewMeetupForm";
 
 function NewMeetupsPage() {
-
-  function addMeetupHandler(dataUserStored){
-    fetch('https://react-project-73830-default-rtdb.asia-southeast1.firebasedatabase.app/meetips.json');
+  function addMeetupHandler(dataUserStored) {
+    fetch(
+      "https://react-project-73830-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(dataUserStored),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
   }
 
   return (
     <section>
       <h1>Add New Meetups</h1>
-      <NewMeetupsForms onAddMeetup = {addMeetupHandler} />
+      <NewMeetupsForms onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
