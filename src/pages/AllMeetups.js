540a9ebbdd89cc90f2982a5bd2ad55fb2f5e713d
jasmeet1233 <jazzratra@gmail.com>
2021-05-25 01:42:30 +0530

    removed dummy data

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,46 +1,28 @@
 import {useEffect, useState} from "react"
 
 import MeetupList from "../components/meetups/MeetList";
-import MeetupItem from "../components/meetups/MeetupItem";
+// import MeetupItem from "../components/meetups/MeetupItem";
 import reactDom from "react-dom";
-
-const DUMMY_DATA = [
-    {
-      id: 'm1',
-      title: 'This is a first meetup',
-      image:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-      address: 'Meetupstreet 5, 12345 Meetup City',
-      description:
-        'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-    {
-      id: 'm2',
-      title: 'This is a second meetup',
-      image:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-      address: 'Meetupstreet 5, 12345 Meetup City',
-      description:
-        'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-  ];
 
 
 function AllMeetupsPage(){
-  const [Isloading, setIsloading] = useState(true)
-  const [LoadedMeetups, setLoadedMeetups] = useState([])
+  const [Isloading, setIsloading] = useState(true);
+  const [LoadedMeetups, setLoadedMeetups] = useState([]);
 
   useEffect(() => {
+    setIsloading(true);
     fetch("https://react-project-73830-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json")
-    .then(response => {
+    .then((response) => {
       return response.json();
     }).then((data) => {
-      const meetups = []
-      for(const key in data) {
+      const meetups = [];
+      
+      for (const key in data) {
         const meetup = {
           id: key,
           ...data[key]
-        }
+        };
+        meetups.push(meetup)
       }
       
       setIsloading(false);
@@ -48,12 +30,10 @@ function AllMeetupsPage(){
     });
   }, []);
 
- 
-
   if (Isloading){
     return (
       <section>
-        Loading...
+        <p>Loading...</p>
       </section>
     )
   }
